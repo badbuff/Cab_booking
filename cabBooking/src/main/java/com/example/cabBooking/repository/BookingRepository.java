7cf01967d6f7b3d7edf0857ecae2e43d8902917c
badbuff <beldaskishan04@gmail.com>
2025-03-13 01:12:05 +0530

    create booking repo

--- cabBooking/src/main/java/com/example/cabBooking/repository/BookingRepository.java
+++ cabBooking/src/main/java/com/example/cabBooking/repository/BookingRepository.java
@@ -1,4 +1,9 @@
 package com.example.cabBooking.repository;
 
-public class BookingRepository {
+import com.example.cabBooking.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 }
