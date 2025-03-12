074130ed83c6e1a03e9f9bf90c45ca42bea2940f
badbuff <beldaskishan04@gmail.com>
2025-03-12 23:02:11 +0530

    Create repo to access the DB

--- cabBooking/src/main/java/com/example/cabBooking/repository/UserRespository.java
+++ cabBooking/src/main/java/com/example/cabBooking/repository/UserRespository.java
@@ -1,2 +1,9 @@
-package com.example.cabBooking.repository;public class UserRespository {
+package com.example.cabBooking.repository;
+
+import com.example.cabBooking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
