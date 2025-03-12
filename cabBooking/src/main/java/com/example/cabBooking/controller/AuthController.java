a225e575c19649ae913f5f1cf003fc1313a07452
badbuff <beldaskishan04@gmail.com>
2025-03-12 23:20:22 +0530

    implement the authentication controller

--- cabBooking/src/main/java/com/example/cabBooking/controller/AuthController.java
+++ cabBooking/src/main/java/com/example/cabBooking/controller/AuthController.java
@@ -1,2 +1,21 @@
-package com.example.cabBooking.controller;public class AuthController {
+package com.example.cabBooking.controller;
+
+import com.example.cabBooking.dto.AuthRequest;
+import com.example.cabBooking.dto.AuthResponse;
+import com.example.cabBooking.service.AuthService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+        return authService.login(request);
+    }
 }
