5a22b71932bd6624211ee0d4bd985214af4d44db
badbuff <beldaskishan04@gmail.com>
2025-03-12 22:38:27 +0530

    Defining user entity

---
+++ cabBooking/src/main/java/com/example/cabBooking/model/User.java
@@ -0,0 +1,27 @@
+package com.example.cabBooking.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
